5f59f871695f5ed5adcd3d5b35bbc2a940d6d7e5
Neerajthind <152557955+Neerajthind@users.noreply.github.com>
2024-08-26 12:06:38 -0700

    Delete 01-setting-up-new-server.sql

--- 01-setting-up-new-server.sql
+++
@@ -1,8 +0,0 @@
--- Drop user first if they exist
-DROP USER if exists 'member_mgmt'@'%' ;
-
--- Now  we create user with prop privileges
-CREATE USER 'member_mgmt'@'%' IDENTIFIED BY 'member_mgmt';
-
--- This will grant all the privileges
-GRANT ALL PRIVILEGES ON * . * TO 'member_mgmt'@'%';
