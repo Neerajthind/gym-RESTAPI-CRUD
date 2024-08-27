a1efe6fc2406f1c8be3b87a47474523b11a29e23
Neerajthind <152557955+Neerajthind@users.noreply.github.com>
2024-08-27 16:50:10 -0700

    Add files via upload
    Finished implementing Spring DATA JPA and REST. This allows less manual configuration on our part. 
    
    Im going to look into more additional features I can add into this application. Possibly a frontend and even a notification system!

--- spring-restapi-w-Data-JPA-REST-crud-app/src/main/java/com/springboot/gymrestapi/dao/MemberRepository.java
+++ spring-restapi-w-Data-JPA-REST-crud-app/src/main/java/com/springboot/gymrestapi/dao/MemberRepository.java
@@ -1,11 +1,13 @@
 package com.springboot.gymrestapi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springboot.gymrestapi.entity.Member;
 
 //TODO --> Using JPA Data to create a DAO for me and all of the CRUD operations
 
+@RepositoryRestResource(path="changed")
 public interface MemberRepository extends JpaRepository<Member, Integer>{
     
 
